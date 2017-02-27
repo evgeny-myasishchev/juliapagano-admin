6e3e10eeb3820faf9c555a6627f03b98b8a65755
Eugene Myasyshchev <Eugene.Myasyshchev@dataart.com>
2017-02-27 10:51:03 +0200

    Using build folder as an output.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+import _ from 'lodash';
+
+function component() {
+  const element = document.createElement('div');
+
+  /* lodash is required for the next line to work */
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = function(env) {
-    return {
-        entry: './app/index.js',
-        output: {
-            filename: '[chunkhash].[name].js',
-            path: './public/assets'
-        },
-        plugins: [new HtmlWebpackPlugin()]
-    }
-}
+module.exports = function () {
+  return {
+    entry: './app/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: './build',
+    },
+    plugins: [new HtmlWebpackPlugin()],
+  };
+};
